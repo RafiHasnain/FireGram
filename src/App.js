7f4ad754dd2622f4365427c3fdb45333aebdfa20
Rafi Hasnain <rafihasnain02@gmail.com>
2021-04-08 23:15:05 +0600

    Adding loading

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
 import React, { useState } from "react";
-import ImageGrid from "./comps/ImageGrid";
+import PulseLoader from "react-spinners/PulseLoader";
 import Modal from "./comps/Modal";
 import Title from "./comps/Title";
 import UploadForm from "./comps/UploadForm";
 import SignIn from "./comps/SignIn";
 import { auth } from "./firebase/config";
+import "firebase/auth";
 import { useAuthState } from "react-firebase-hooks/auth";
 import SignOut from "./comps/SignOut";
+
+// const ImageGrid = React.lazy(() => import("./comps/ImageGrid")); // Lazy-loaded
+import ImageGrid from "./comps/ImageGrid";
+
 function App() {
   const [selectedImg, setSelectedImg] = useState(null);
+  const [Loading, setLoading] = useState(false);
+
+  const loader = () => {
+    setLoading(false);
+  };
   const [user] = useAuthState(auth);
+
   return (
     <div className='App'>
       <header>
@@ -23,7 +34,17 @@ function App() {
           <div>
             <Title />
             <UploadForm />
-            <ImageGrid setSelectedImg={setSelectedImg} />
+            {Loading ? (
+              <div className='loading'>
+                <PulseLoader color={"#a18cd1"} loading={Loading} size={150} />
+              </div>
+            ) : (
+              <ImageGrid
+                setSelectedImg={setSelectedImg}
+                setLoading={setLoading}
+                loader={loader}
+              />
+            )}
             {selectedImg && (
               <Modal
                 selectedImg={selectedImg}
--- src/comps/ImageGrid.js
+++ src/comps/ImageGrid.js
@@ -1,29 +1,52 @@
-import React from "react";
+import React, { useEffect } from "react";
 import useFirestore from "../hooks/useFirestore";
 import { motion } from "framer-motion";
+// Lazy-loaded
 
-const ImageGrid = ({ setSelectedImg }) => {
+const ImageGrid = ({ setSelectedImg, setLoading, loader }) => {
   const { docs } = useFirestore("images");
-  console.log(docs);
+  // console.log(docs);
+
+  useEffect(() => {
+    if (docs !== undefined || docs.length !== 0) {
+      console.log(docs);
+      // setLoading(true);
+      // loader();
+      // console.log("hello");
+    }
+  }, [docs, loader]);
+
   return (
-    <div className='img-grid'>
-      {docs &&
-        docs.map((doc) => (
-          <motion.div
-            className='img-wrap'
-            key={doc.id}
-            layout
-            whileHover={{ opacity: 1 }}
-            onClick={() => setSelectedImg(doc.url)}>
-            <motion.img
-              src={doc.url}
-              alt='uploaded pic'
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1 }}
-              transition={{ delay: 1 }}
-            />
-          </motion.div>
-        ))}
+    <div>
+      <div className='img-grid'>
+        {docs &&
+          docs.map((doc) => (
+            // <motion.div
+            //   className='img-wrap'
+            //   key={doc.id}
+            //   layout
+            //   whileHover={{ opacity: 0 }}
+            //   onClick={() => setSelectedImg(doc.url)}>
+            //   <motion.img
+            //     src={doc.url}
+            //     alt='uploaded pic'
+            //     initial={{ opacity: 0 }}
+            //     animate={{ opacity: 1 }}
+            //     transition={{ delay: 0 }}
+            //   />
+            // </motion.div>
+
+            <div
+              className='img-wrap'
+              key={doc.id}
+              layout
+              // whileHover={{ opacity: 0 }}
+              // onLoad={() => setLoading(false)}
+              onClick={() => setSelectedImg(doc.url)}>
+              <img src={doc.url} alt='uploaded pic' />
+            </div>
+          ))}
+      </div>
     </div>
   );
 };
